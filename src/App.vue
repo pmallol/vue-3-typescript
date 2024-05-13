5830573ac14be79da9afb147109d4f57e92a5686
pmallol <paulamallol@gmail.com>
2024-05-13 00:06:59 -0300

    refactor: Update App.vue to use consistent spacing and formatting

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { ref, reactive } from 'vue';
+import { ref, reactive} from 'vue'
 
 interface AppInfo {
-    name: string;
-    description: string;
+  name: string
+  description: string
 }
 
-const count = ref(0);
+const count = ref(0)
 
-const appInfo: AppInfo = reactive({ 
+const appInfo: AppInfo = reactive({
   name: 'Counter',
-  description: 'An app you can count on - made with vue + ts' }
-);
+  description: 'An app you can count on - made with vue + ts'
+})
+
+function addCount(num: number) {
+  if (count.value !== null) count.value += num
+}
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <h1>{{appInfo.name}}</h1>
-      <h2>{{appInfo.description}}</h2>
+      <h1>{{ appInfo.name }}</h1>
+      <h2>{{ appInfo.description }}</h2>
     </div>
-    <p>{{count}}</p>
+    <p>{{ count }}</p>
+    <button @click="addCount">Add count</button>
   </header>
 
   <RouterView />
