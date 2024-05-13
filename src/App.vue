2a6ebf164b79214ce258e0a1b027bb5b96f153a4
pmallol <paulamallol@gmail.com>
2024-05-13 00:03:14 -0300

    chore: Add initial project files and configurations

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { ref, reactive } from 'vue';
+
+interface AppInfo {
+    name: string;
+    description: string;
+}
+
+const count = ref(0);
+
+const appInfo: AppInfo = reactive({ 
+  name: 'Counter',
+  description: 'An app you can count on - made with vue + ts' }
+);
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>{{appInfo.name}}</h1>
+      <h2>{{appInfo.description}}</h2>
+    </div>
+    <p>{{count}}</p>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+</style>
